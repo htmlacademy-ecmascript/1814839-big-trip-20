944c6717de11cba34a6b4e841a243f83bd328d5a
lynx-boop <shimukurumhi@gmail.com>
2023-04-26 17:53:01 +0300

    добавляет компоненты фильтра, сортировкиб загрузки
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { render } from './render.js';
 import NewEventButtonView from './view/new-event-button-view.js';
+import FilterView from './view/filter-view.js';
+import SortView from './view/sort-view.js';
 
 const header = document.querySelector('.page-header');
+const tripMain = header.querySelector('.trip-main');
+const tripControlsFilters = header.querySelector('.trip-controls__filters');
+const main = document.querySelector('.page-main');
+const tripEvents = main.querySelector('.trip-events');
 
 const eventButton = new NewEventButtonView();
+const filter = new FilterView();
+const sort = new SortView();
 
-render(eventButton, header);
+render(eventButton, tripMain);
+render(filter, tripControlsFilters);
+render(sort, tripEvents);
