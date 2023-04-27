ff142a70f1664bf7c9cb21e75512c2b039aaeec6
lynx-boop <shimukurumhi@gmail.com>
2023-04-27 15:28:26 +0300

    создает вьюхи добавления и редактирования
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { render } from './render.js';
 import NewEventButtonView from './view/new-event-button-view.js';
 import FilterView from './view/filter-view.js';
 import SortView from './view/sort-view.js';
+import AddNewPointView from './view/add-new-point-view.js';
 
 const header = document.querySelector('.page-header');
 const tripMain = header.querySelector('.trip-main');
@@ -12,7 +13,9 @@ const tripEvents = main.querySelector('.trip-events');
 const eventButton = new NewEventButtonView();
 const filter = new FilterView();
 const sort = new SortView();
+const addNewPoint = new AddNewPointView();
 
 render(eventButton, tripMain);
 render(filter, tripControlsFilters);
 render(sort, tripEvents);
+render(addNewPoint, tripEvents);
