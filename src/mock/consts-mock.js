7806509e9af227c7508680229c24de990c864092
lynx-boop <shimukurumhi@gmail.com>
2023-06-29 20:49:02 +0300

    добавляет сервис для работы с моками
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- src/mock/consts-mock.js
+++ src/mock/consts-mock.js
@@ -9,11 +9,27 @@ const CITIES = [
   'Rotterdam'
 ];
 
+const TYPES = [
+  'taxi',
+  'bus',
+  'train',
+  'flight',
+  'check-in',
+  'sightseeing',
+  'ship',
+  'drive',
+  'restaurant'
+];
+
 const DESCRIPTION = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+const DESTINATION_COUNT = 5;
+const OFFER_COUNT = 9;
+const POINT_COUNT = 5;
 
 const Price = {
   MIN: 100,
   MAX: 10000
 };
 
-export { CITIES, DESCRIPTION, Price };
+export { CITIES, DESCRIPTION, DESTINATION_COUNT, OFFER_COUNT, POINT_COUNT, TYPES, Price };
---
+++ src/service/mock-service.js
@@ -0,0 +1,68 @@
+import { DESTINATION_COUNT, OFFER_COUNT, POINT_COUNT, TYPES } from '../mock/consts-mock.js';
+import { getRandomInteger, getRandomArrayElement } from '../utils.js';
+import { generateDestination } from '../mock/destination-mock.js';
+import { generateOffer } from '../mock/offer-mock.js';
+import { generatePoint } from '../mock/point-mock.js';
+
+export default class MockService {
+  destinations = [];
+  offers = [];
+  points = [];
+
+  constructor() {
+    this.destinations = this.generateDestinationsArray;
+    this.offers = this.generateOffersArray;
+    this.points = this.generatePointsArray;
+  }
+
+  getDestinations() {
+    return this.destinations;
+  }
+
+  getOffers() {
+    return this.offers;
+  }
+
+  getPoints() {
+    return this.points;
+  }
+
+  generateDestinationsArray() {
+    return Array.from(
+      { length: DESTINATION_COUNT },
+      () => generateDestination()
+    );
+  }
+
+  generateOffersArray() {
+    return TYPES.map((type) => ({
+      type,
+      offers: Array.from(
+        { length: getRandomInteger(0, OFFER_COUNT) },
+        () => generateOffer(type))
+    }));
+  }
+
+  // не понимаю, каким образом работает этот код, обьясни плз
+  generatePointsArray() {
+    return Array.from(
+      { length: POINT_COUNT },
+      () => {
+        const type = getRandomArrayElement(TYPES);
+        const destination = getRandomArrayElement(this.destinations);
+
+        const hasOffers = getRandomInteger(0, 1);
+
+        const offersByType = this.offers.find((offerByType) => offerByType.type === type);
+
+        const offersIds = (hasOffers)
+          ? offersByType.offers
+            .slice(0, OFFER_COUNT)
+            .map((offer) => offer.id)
+          : [];
+        return generatePoint(type, destination.id, offersIds);
+      }
+    );
+  }
+
+}
