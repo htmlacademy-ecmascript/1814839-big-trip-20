27ac2f9046855e1796b7bf4182f8f0dae818c525
lynx-boop <shimukurumhi@gmail.com>
2023-08-01 13:39:40 +0300

    добавляет получение офферов
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- src/mock/offer-mock.js
+++ src/mock/offer-mock.js
@@ -1,11 +1,11 @@
-import { getRandomInteger } from '../utils.js';
-import { Price } from './consts-mock.js';
+import { getRandomInteger, getRandomArrayElement } from '../utils.js';
+import { Price, OFFER_TITLES } from './consts-mock.js';
 
 export default function generateOffer() {
   const id = crypto.randomUUID();
   return {
     id: id,
-    title: `${id} title`,
-    price: getRandomInteger(Price.MIN, Price.MAX)
+    title: getRandomArrayElement(OFFER_TITLES),
+    price: `${getRandomInteger(Price.MIN, Price.MAX / 10)} €`
   };
 }
--- src/view/trip-point-view.js
+++ src/view/trip-point-view.js
@@ -1,17 +1,31 @@
 import { createElement } from '../render.js';
+import { CITIES } from '../mock/consts-mock.js';
+import { getRandomArrayElement } from '../utils.js';
+import MockService from '../service/mock-service.js';
 
-//не знаю, как прокинуть в темплейт isFavorite и offers
-const getTemplate = (point, pointDestination, pointOffers) => {
-  const { type, basePrice, isFavorite, offers } = point;
+const getOffers = (type) => {
+  const mockService = new MockService();
+  const offersByType = mockService.offers.find((offer) => offer.type === type).offers;
 
-  return (/*html*/
-    `<li class="trip-events__item">
+  return offersByType.map(
+    (offer) => `<li class="event__offer" >
+      <span class="event__offer-title">${offer.title}</span>
+    <span class="event__offer-price">${offer.price}</span>
+</li> `
+  ).join('');
+};
+
+const getTemplate = (point) => {
+  const name = getRandomArrayElement(CITIES);
+  const { type, basePrice, isFavorite } = point;
+
+  return `<li class="trip-events__item" >
   <div class="event">
     <time class="event__date" datetime="2019-03-18">MAR 18</time>
     <div class="event__type">
       <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event ${type} icon">
     </div >
-    <h3 class="event__title">${type}</h3>
+    <h3 class="event__title">${type} ${name}</h3>
     <div class="event__schedule">
       <p class="event__time">
         <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
@@ -25,36 +39,30 @@ const getTemplate = (point, pointDestination, pointOffers) => {
     </p>
     <h4 class="visually-hidden">Offers:</h4>
     <ul class="event__selected-offers">
-      <li class="event__offer">
-        <span class="event__offer-title">Order Uber</span>
-        &plus;&euro;&nbsp;
-        <span class="event__offer-price">20</span>
-      </li>
-    </ul>
-    <button class="event__favorite-btn event__favorite-btn--active" type="button">
+      ${getOffers(type)}
+    </ul >
+    <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''} " type="button">
       <span class="visually-hidden">Add to favorite</span>
       <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-        <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+        <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z" />
       </svg>
     </button>
     <button class="event__rollup-btn" type="button">
       <span class="visually-hidden">Open event</span>
     </button>
   </div >
-</li > `);
+</li> `;
 };
 
 
 export default class TripPointView {
-  constructor({ point, pointDestination, pointOffers }) {
+  constructor({ point }) {
     this.point = point;
-    this.pointDestination = pointDestination;
-    this.pointOffers = pointOffers;
   }
 
   getElement() {
     if (!this.element) {
-      this.element = createElement(getTemplate(this.point, this.pointDestination, this.pointOffers));
+      this.element = createElement(getTemplate(this.point));
     }
 
     return this.element;
