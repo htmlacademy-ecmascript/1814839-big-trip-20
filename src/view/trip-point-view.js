22db2919f058a621c38460be32a14dabaf3b9bae
lynx-boop <shimukurumhi@gmail.com>
2023-08-11 18:45:31 +0300

    подключает рандомизатор даты
    Signed-off-by: lynx-boop <shimukurumhi@gmail.com>

--- src/view/trip-point-view.js
+++ src/view/trip-point-view.js
@@ -17,20 +17,20 @@ const getOffers = (type) => {
 
 const getTemplate = (point) => {
   const name = getRandomArrayElement(CITIES);
-  const { type, basePrice, isFavorite } = point;
+  const { type, basePrice, isFavorite, dateFrom, dateTo } = point;
 
   return `<li class="trip-events__item" >
   <div class="event">
-    <time class="event__date" datetime="2019-03-18">MAR 18</time>
+    <time class="event__date" datetime="2019-03-18">${dateFrom}</time>
     <div class="event__type">
       <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event ${type} icon">
     </div >
     <h3 class="event__title">${type} ${name}</h3>
     <div class="event__schedule">
       <p class="event__time">
-        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+        <time class="event__start-time" datetime="2019-03-18T10:30">${dateFrom}</time>
         &mdash;
-        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+        <time class="event__end-time" datetime="2019-03-18T11:00">${dateTo}</time>
       </p>
       <p class="event__duration">30M</p>
     </div>
@@ -47,7 +47,7 @@ const getTemplate = (point) => {
         <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z" />
       </svg>
     </button>
-    <button class="event__rollup-btn" type="button"> //на вот эту кнопку навешиваются евент листнеры
+    <button class="event__rollup-btn" type="button">
       <span class="visually-hidden">Open event</span>
     </button>
   </div >
